325143f13a0c35521baa8ea825aa2687dbed76a9
sofiaa21 <91670510+sofiaa21@users.noreply.github.com>
2025-10-26 19:04:15 +0100

    working merkle tree and main test

--- MerkleTree.py
+++ MerkleTree.py
@@ -1,9 +1,55 @@
 import hashlib 
+import math
 
-LAMBDA = 256
-class MerkleTree:
-    def init_hash(message_leaf):
-        return hashlib.shake_256(message_leaf.encode("utf-8")).hexdigest(LAMBDA)
+LAMBDA_BYTES = 32   # 256 bits
 
+class MerkleTree:        
+    def tree_hash(data_bytes):
+        hash_obj =  hashlib.shake_256(data_bytes)
+        return hash_obj.digest(LAMBDA_BYTES)
+
+    def build_tree(message, salt):
+        # convert into bits
+        message_bits = len(message) * 8
+        lambda_bits = 256
+
+        # If the length of message is smaller or same as lambda 
+        # the algorithm simply outputs the message m as the hash
+        if message_bits <=lambda_bits:
+            return message
+
+        # Define our l
+        l = math.log2(len(message)/LAMBDA_BYTES)
+        # If l == 0, redefine as l = 1, so we can use the 2**l operation
+        if l==0:
+            l = 1
+        
+        estimated_length_bytes = ((2**l) *lambda_bits)//8
+
+        # Define padding
+        padding_len_bytes = int(estimated_length_bytes - len(message))
+        padding = b'\x00' * padding_len_bytes
+        m_prime = message + padding
+        # We hash the message blocks
+        hashes = [] 
+        # How many hashes are there to compute (use hash.ceil to avoid floating errors)
+        hashes_count = math.ceil((2**(l-1)))
+
+        for i in range(hashes_count):
+            start = i * (2*LAMBDA_BYTES)
+            end = start + (2*LAMBDA_BYTES)
+
+            data_block = m_prime[start:end]
+
+            #Add salt to hash input s || data_block
+            hash_input = salt + data_block
+
+            hash_i = MerkleTree.tree_hash(hash_input)
+            hashes.append(hash_i)
+        
+        concat_hashes = b''.join(hashes)
+        
+        # Hash the concatenated message to move to next tree level
+        return MerkleTree.build_tree(concat_hashes,salt)
 
 
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-import hashlib
 import MerkleTree
+import os
 
 if __name__ == "__main__":
 
     Merkle_Tree_obj = MerkleTree.MerkleTree
-    m = "hello"
-    hashed_m = Merkle_Tree_obj.init_hash(m)
-    print(hashed_m)
+    m = "Once when I was six years old I saw a magnificent picture in a book, called True Stories from Nature, about the primeval forest. It was a picture of a boa constrictor in the act of swallowing an animal. Here is a copy of the drawing."
+    m_bytes = m.encode('utf-8')
+    
+    print(f"Original message: {m}")
+    print(f"Message length: {len(m)} bytes")
+
+    # Generate random 32 byte string of bytes
+    salt = os.urandom(32)
+    print(f"Salt that will be used in the Merkle tree (HEX): {salt.hex()}")
+    print("------------------------------------------------")
+    
+    try:
+        final_hash = Merkle_Tree_obj.build_tree(m_bytes,salt)
+        print(f"Final Merkle Root: {final_hash.hex()}")
+        print(f"Hash length: {len(final_hash)} bytes")
+    except ValueError as e:
+        print(f"Error: {e}")
+        print("Please ensure your salt is exactly 32 bytes long.")
